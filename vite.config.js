893642c03874d30ae5adfdb3cfcb497835e5237d
Josh Hill <joshuaamhill@gmail.com>
2024-08-29 17:56:33 -0700

    removing test

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-  // base: "/Worldwise-Travel-App/",
+  base: "/Worldwise-Travel-App/",
 });
