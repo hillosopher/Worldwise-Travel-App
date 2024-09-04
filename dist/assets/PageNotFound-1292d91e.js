a722d04d81278bd7207387c294bf8ec9608440a3
Josh Hill <joshuaamhill@gmail.com>
2024-09-04 14:03:01 -0700

    rebuild

---
+++ dist/assets/PageNotFound-1292d91e.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-c45b837a.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
--- dist/assets/PageNotFound-546a291c.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-c7028ec5.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
